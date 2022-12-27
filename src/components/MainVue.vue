10a5f5884f9833f2e3935a534f379c58c4423bb8
Ibrahim <ibrachar68@gmail.com>
2022-12-27 12:25:01 +0100

    creazione header parte main

--- src/components/MainVue.vue
+++ src/components/MainVue.vue
@@ -11,6 +11,14 @@ export default {
 <template>
     <div class="container">
         <div class="header">
+            <div class="search">
+                <a href=""> <font-awesome-icon icon="fa-solid fa-angle-left" class="mrg" /></a>
+                <a href=""> <font-awesome-icon icon="fa-solid fa-angle-right" class="mrg" /></a>
+                <input type="text" placeholder="Search for Artists, Songs, or Podcasts">
+            </div>
+            <div class="up">
+                <a href=""><span class="upgrade">EFFETTUA L`UPGRADE</span></a>
+            </div>
 
         </div>
 
@@ -20,14 +28,64 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-    background-color: aquamarine;
+    background-color: #141922;
     width: calc(100% - 250px);
     height: calc(100vh - 80px);
 
     .header {
         width: 100%;
         height: 80px;
-        background-color: brown;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        // position: fixed;
+        // top: 0;
+        // left: 0;
+        .search {
+            margin-left: 20px;
+
+            a {
+                text-decoration: none;
+                color: white;
+                font-size: 25px;
+
+                .mrg {
+                    margin-right: 30px;
+                    vertical-align: middle;
+                }
+
+            }
+
+            input {
+                padding: 10px 8px;
+                width: 300px;
+                outline: none;
+                border-radius: 15px;
+                background-image: url("https://www.svgrepo.com/show/13652/magnifying-glass.svg");
+                background-size: 13px;
+                background-repeat: no-repeat;
+                background-position: 10px center;
+                padding-left: 30px;
+            }
+        }
+
+        a {
+            text-decoration: none;
+
+            .upgrade {
+                color: white;
+                font-size: 15px;
+                padding: 10px 30px;
+                border-radius: 20px;
+                border: 1px solid white;
+                margin-right: 20px;
+
+            }
+
+        }
+
     }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBackwardStep, faBars, faCirclePlay, faDesktop, faFolder, faForwardStep, faHatWizard, faHeart, faHouse, faMagnifyingGlass, faRotateRight, faShuffle, faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft, faAngleRight, faBackwardStep, faBars, faChevronLeft, faChevronRight, faCirclePlay, faDesktop, faFolder, faForwardStep, faHatWizard, faHeart, faHouse, faMagnifyingGlass, faRotateRight, faShuffle, faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-library.add(faHatWizard, faHouse, faMagnifyingGlass, faBars, faDesktop, faVolumeHigh, faHeart, faFolder, faShuffle, faBackwardStep, faCirclePlay, faForwardStep, faRotateRight)
+library.add(faHatWizard, faHouse, faMagnifyingGlass, faBars, faDesktop, faVolumeHigh, faHeart, faFolder, faShuffle, faBackwardStep, faCirclePlay, faForwardStep, faRotateRight, faAngleLeft, faAngleRight)
 
 
 createApp(App)
